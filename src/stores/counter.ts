c8d51421a42ab1b13b6c0e9b996de0cb1aed7114
yjh <langbiantianya@outlook.com>
2025-04-30 17:32:31 +0800

    feat: add login page

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -4,9 +4,13 @@ import { defineStore } from 'pinia'
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
+  // 添加 token 状态
+  const token = ref('')
+
   function increment() {
     count.value++
   }
 
-  return { count, doubleCount, increment }
+  // 返回 token
+  return { count, doubleCount, increment, token }
 })
